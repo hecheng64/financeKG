8a4c13b2337896d07e449999a03ba661fabdbc33
CaiWZ <905137015@qq.com>
2021-03-05 17:57:56 +0800

    commit second methods

--- src/main/java/janus/schema/enuminfo/MixedIndexType.java
+++
@@ -1,5 +0,0 @@
-package com.cc.janus.schema.enuminfo;
-
-public enum MixedIndexType {
-    search,solr,lucene;
-}
--- src/main/java/janus/task/Task.java
+++
@@ -1,7 +0,0 @@
-package com.cc.janus.task;
-
-import java.util.Map;
-
-public interface Task {
-    public void execute(Map<String,Object> options);
-}
